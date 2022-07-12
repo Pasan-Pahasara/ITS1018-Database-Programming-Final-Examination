d88d07b5847556e8e084078ee9d442a7c94e368c
unknown <pasanpahasara7788@gmail.com>
2022-07-12 11:15:27 +0530

    UI Bug Fix

--- src/controller/StudentManageFormController.java
+++ src/controller/StudentManageFormController.java
@@ -1,42 +1,53 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import model.Student;
 
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
 public class StudentManageFormController {
-    public TextField txtSID;
-    public TextField txtSContact;
-    public TextField txtSNic;
-    public TextField txtSAddress;
-    public TextField txtSEmail;
-    public TextField txtSName;
-    public TableView tblStudent;
-    public TextField txtSearch;
-    public JFXButton btnDelete;
+    public AnchorPane root;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtStudentContact;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentNic;
+    public TableView <Student>tblStudents;
+    public JFXButton btnAddNew;
     public JFXButton btnSave;
+    public JFXButton btnDelete;
 
-    public void save(ActionEvent actionEvent) {
+    /**
+     * Initializes the controller class.
+     */
+    public void initialize(){
+        tblStudents.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        tblStudents.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("studentName"));
+        tblStudents.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("email"));
+        tblStudents.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("contact"));
+        tblStudents.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("address"));
+        tblStudents.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("nic"));
+
+        loadAllStudents();
     }
 
-    public void textFields_Key_Released(KeyEvent keyEvent) {
+    private void loadAllStudents() {
     }
 
-    public void txtSearchOnAction(ActionEvent actionEvent) {
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
     }
 
-    public void updateBtnOnAction(ActionEvent actionEvent) {
+    public void btnSave_OnAction(ActionEvent actionEvent) {
     }
 
-    public void deleteBtnOnAction(ActionEvent actionEvent) {
-    }
-
-    public void saveBtnOnAction(ActionEvent actionEvent) {
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
     }
 }
